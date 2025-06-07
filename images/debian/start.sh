af5f679f26cee576245c09066b603e12a9242264
CyberL1 <cyber.hf18@gmail.com>
2025-06-07 18:26:10 +0200

    feat: create user and use it

--- images/debian/start.sh
+++ images/debian/start.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-echo 'root:root' | chpasswd
-sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-service ssh start
+sudo service ssh start
 
 # Required to keep the container running
 tail -f /dev/null
